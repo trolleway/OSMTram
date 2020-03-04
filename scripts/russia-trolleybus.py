f3d1c98be31212648b3dabe39f8255eb9f93b831
Artem Svetlov <trolleway@yandex.ru>
2020-03-04 18:49:42 +0300

    Update russia-trolleybus.py

--- scripts/russia-trolleybus.py
+++ scripts/russia-trolleybus.py
@@ -21,6 +21,7 @@ def argparser_prepare():
             formatter_class=PrettyFormatter)
     parser.add_argument('--prune',dest='prune', required=False, action='store_true', help='Clear temporary folder')
     parser.add_argument('--skip-osmupdate',dest='skip_osmupdate', required=False, action='store_true')
+    parser.add_argument('--workdir',dest='WORKDIR', required=True)
 
     parser.epilog = \
         '''Samples:
@@ -31,7 +32,7 @@ def argparser_prepare():
     return parser
 
 
-WORKDIR='/media/trolleway/elvideo/osmtram'
+
 POLY='russia-trolleybus.poly'
 dump_url = 'http://download.geofabrik.de/russia/northwestern-fed-district-latest.osm.pbf'
 dump_url = 'http://download.geofabrik.de/russia/volga-fed-district-latest.osm.pbf'
@@ -40,7 +41,7 @@ dump_url = 'http://download.geofabrik.de/russia-latest.osm.pbf'
 parser = argparser_prepare()
 args = parser.parse_args()
 
-
+WORKDIR=args.WORKDIR
 
 
 logging.basicConfig(level=logging.DEBUG,format='%(asctime)s %(levelname)-8s %(message)s',datefmt='%Y-%m-%d %H:%M:%S')
